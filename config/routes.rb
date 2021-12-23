6d13502340c147369dcbb5f1a1bce07e13ee8a60
Merjen Meredova <MeredovaM@stud.kai.ru>
2021-12-23 16:26:29 +0300

    added sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
-  get 'welcome/index'
-  get 'contact' => 'static_pages#contact'
-  root 'static_pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ root 'static_pages#home'
+ get 'help' => 'static_pages#help'
+ get 'about' => 'static_pages#about'
+ get 'contact' => 'static_pages#contact'
+ get 'signup' => 'users#new'
 end
